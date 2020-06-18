7d48711cb333058fef0f9c46b0a3d9844ee28fd7
unknown <kamalaujla4vr@gmail.com>
2020-06-18 13:38:56 +0530

    Uploading initial project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { GoogleChartInterface } from 'ng2-google-charts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-google-charts';
+  
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Ng2GoogleChartsModule, GoogleChartsSettings } from 'ng2-google-charts';
+import { PiechartComponent } from './piechart/piechart.component';
+import { GeochartComponent } from './geochart/geochart.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PiechartComponent,
+    GeochartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2GoogleChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
